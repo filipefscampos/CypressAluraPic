f567aec08bd7426cc9a2c51228684077a8f1c830
Filipe Campos <felipetwo@gmail.com>
2022-06-28 17:31:27 -0300

    adicionando teste em dados sensiveis cypress.env.json

--- cypress/e2e/3-alurapic/api-alurapic.cy.js
+++ cypress/e2e/3-alurapic/api-alurapic.cy.js
@@ -12,4 +12,19 @@ describe('buscar fotos e dados por api', () => {
         })
     });
 
+    it.only('fazer login do filipe', () => {
+        cy.request({
+            method: 'POST',
+            url: 'https://apialurapic.herokuapp.com/user/login',
+            body: Cypress.env()
+        }).then((res) => {
+            expect(res.status).to.be.equal(200)
+            expect(res.body).is.not.empty
+            expect(res.body).to.have.property('id')
+            expect(res.body.id).to.be.equal(13)
+            expect(res.body).to.have.property('email')
+            expect(res.body.email).to.be.equal("felipetwo@gmail.com")
+        })
+    });
+
 });
